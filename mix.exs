b7c54629d2464c8646dbb07b7a3703060d742101
feng19 <kevin@feng19.com>
2024-12-24 10:17:21 +0800

    udpate deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XTrace.MixProject do
   def project do
     [
       app: @app,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
